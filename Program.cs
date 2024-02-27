1acc12b26baee9bed38e1c3a44ac336be45c8c87
fgdsgfdg <156791585+fgdsgfdg@users.noreply.github.com>
2024-02-27 11:10:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            int x;
+            Console.WriteLine("Число: \n");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"y = {Math.Sqrt(x) + 10}");
+        }
+    }
+}
